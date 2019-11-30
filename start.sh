65fce8f526d3adace155f4eaf20d95da81f44107
nerdymark <nerdymark@gmail.com>
2019-11-30 11:46:55 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,4 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-jupyter notebook --ip='*' --no-browser /deepdream/deepdream
+jupyter notebook --ip='0.0.0.0' --no-browser /deepdream/deepdream
